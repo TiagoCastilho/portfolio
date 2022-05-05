aacbc7085716f57f1fc73b4d1a5257a05918ddf8
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-05-05 11:09:27 -0300

    Ajuste de responsividade na Home

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,8 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import menuImg from '../../assets/menu.png';
 
 export const Menu = () => {
+    const [toogle, setToogle] = useState(true);
+    const [show, setShow] = useState('none');
+    useEffect(() => {
+      setShow((state) => toogle ? 'none': 'block');
+    }, [toogle]);
+
     return(
         <header>
             <div className="header">
@@ -10,8 +17,15 @@ export const Menu = () => {
                     <div className="logoimg"></div>
                 </div>
                 <div className="menu">
-                    <img src="./assets/menu.png" alt="" />
-                    <nav>
+                    <img 
+                        onClick={e=>setToogle(state => !state)}
+                        className="menu-opener" 
+                        src={menuImg} 
+                        alt=""
+                    />
+                    <nav style={{
+                            display:show,
+                    }}>
                         <ul>
                             <li className="active"><Link to="/">Home</Link></li>
                             <li><Link to="/about">Quem somos</Link></li>
